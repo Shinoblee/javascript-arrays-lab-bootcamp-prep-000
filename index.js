dc5dd809422eb2cfe492067e1f66e7b92d6a300f
Lee Simon <shinoblee@gmail.com>
2018-08-02 16:26:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+} 
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+ return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
